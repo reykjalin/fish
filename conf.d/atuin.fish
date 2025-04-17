b4169852b0ccb46404fb6b2b1e72465373206ff8
Kristofer Thorlaksson <kristofer.thorlaksson@shopify.com>
2025-04-17 19:47:12 -0400

    fix atuin conf

--- conf.d/atuin.fish
+++ conf.d/atuin.fish
@@ -1,4 +1,4 @@
-if type --query autin; and status is-interactive
+if type --query atuin; and status is-interactive
     # Commands to run in interactive sessions can go here
     atuin init fish | source
 end
