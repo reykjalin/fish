b20aefa542b0bbf082660291c33bba7ff8971157
Kristofer Thorlaksson <kristofer.thorlaksson@shopify.com>
2025-04-17 20:03:50 -0400

    improve editor helper

--- functions/e.fish
+++ functions/e.fish
@@ -1,7 +1,34 @@
-function e --wraps='hx (fd --type f | zf)' --description 'alias e hx (fd --type f | zf)'
-    if count $argv >/dev/null
+function e --description 'opens a fuzzy file selector to open your editor if available'
+    set fuzzer ''
+    if type --query zf
+        set fuzzer zf
+    else if type --query fzf
+        set fuzzer fzf
+    end
+
+    # If a fuzzer isn't available we don't try it and just run the editor.
+    if not test -n "$fuzzer"
         $EDITOR $argv
+        return
+    end
+
+    # We use `fd` if available.
+    set find find . -type f
+    if type --query fd
+        set find fd --type f
+    end
+
+    if count $argv >/dev/null
+        # If a directory is provided, we move there first.
+        # Otherwise: open the provided file in the editor.
+        if test -d $argv
+            cd $argv
+            $EDITOR ($find | $fuzzer)
+        else
+            $EDITOR $argv
+        end
     else
-        $EDITOR (fd --type f | zf)
+        # If nothing is provided, open a picker for files in the current directory.
+        $EDITOR ($find | $fuzzer)
     end
 end
