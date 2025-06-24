ed90f4e46ccf648d79083d3b44b29afc5c8de130
Kristofer Thorlaksson <kristofer.thorlaksson@shopify.com>
2025-06-23 22:06:36 -0400

    use difftastic for git diff when available

--- conf.d/abbreviations.fish
+++ conf.d/abbreviations.fish
@@ -1,19 +1,23 @@
 # Git.
 abbr --add ga git add
 abbr --add gap git add --patch
-abbr --add gd git diff
 abbr --add gp git push
 abbr --add gpf git push --force
 abbr --add gpl git pull
 abbr --add gplr git pull --rebase
 abbr --add gs git status
+if type --query difft
+    abbr --add gd git -c diff.external=difft diff
+else
+    abbr --add gd git diff
+end
 
 function remote_and_branch
     set branch (git rev-parse --abbrev-ref HEAD)
     echo (git config --get branch.$branch.remote) $branch
 end
 
-abbr --command git ob --function remote_and_branch
+abbr --command git cb --function remote_and_branch
 
 # Zig build.
 abbr --add zb zig build
