09b5dca36bedb8d94d26b1964bd38764e8733530
Kristófer R <kristofer@thorlaksson.com>
2025-07-11 23:00:33 -0400

    update j and add completions

---
+++ completions/j.fish
@@ -0,0 +1,1 @@
+complete -c j -a "(__fish_complete_cd)"
--- functions/j.fish
+++ functions/j.fish
@@ -1,8 +1,23 @@
 function j
+    set old_cwd $PWD
+    if test -n "$argv[1]"
+        cd "$argv[1]"
+        set should_alter_history yes
+    end
+
     set dir (fd . --type d --max-depth 4 | zf --height 40 --preview 'eza --icons -lh ')
     if test -z "$dir"
+        cd "$old_cwd"
+        if test -n "$should_alter_history"
+            set dirprev $dirprev[1..-3]
+        else
+            set dirprev $dirprev[1..-2]
+        end
         return 1
     end
 
     cd $dir
+    if test -n "$should_alter_history"
+        set dirprev $dirprev[1..-2]
+    end
 end
